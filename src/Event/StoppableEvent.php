efbddbc6ee4cfd121d8e8fced4dcd125ec344051
Maxime <maxime@maximebatista.lycee@gmail.com>
2024-02-14 10:39:44 +0100

    :bookmark: Launch Event Dispatcher

---
+++ src/Event/StoppableEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Aatis\EventDispatcher\Event;
+
+use Aatis\EventDispatcher\Interface\StoppableEventInterface;
+
+abstract class StoppableEvent extends Event implements StoppableEventInterface
+{
+    private bool $propagationStopped = false;
+
+    public function isPropagationStopped(): bool
+    {
+        return $this->propagationStopped;
+    }
+
+    public function stopPropagation(): static
+    {
+        $this->propagationStopped = true;
+
+        return $this;
+    }
+}
--- src/Service/ListenerProvider.php
+++ src/Service/ListenerProvider.php
@@ -2,39 +2,176 @@
 
 namespace Aatis\EventDispatcher\Service;
 
-use Aatis\DependencyInjection\Interface\ContainerInterface;
-use Aatis\EventDispatcher\Entity\Event;
-use Aatis\EventDispatcher\Interface\ListenerInterface;
+use Aatis\EventDispatcher\Event\Event;
 use Psr\EventDispatcher\ListenerProviderInterface;
+use Aatis\DependencyInjection\Interface\ContainerInterface;
+use Aatis\EventDispatcher\Attribute\EventListener;
+use Aatis\EventDispatcher\Interface\EventSubscriberInterface;
+use Aatis\EventDispatcher\Exception\ListenerProvider\InvalidArgumentException;
+use Aatis\EventDispatcher\Exception\ListenerProvider\InvalidListenerArgumentException;
 
 class ListenerProvider implements ListenerProviderInterface
 {
+    /** @var EventSubscriberInterface[] */
+    private array $subscribers = [];
+
     /**
-     * @var iterable<ListenerInterface>
+     * @var array<class-string, array<array{
+     *  listener: object&callable,
+     *  priority: int
+     * }>>
      */
-    private iterable $listeners = [];
+    private array $listeners = [];
 
     public function __construct(private readonly ContainerInterface $container)
     {
-        /** @var ListenerInterface $listener */
-        foreach ($this->container->getByInterface(ListenerInterface::class) as $listener) {
+        $this->addSubscribers();
+        $this->addListeners();
+    }
+
+    private function addSubscribers(): void
+    {
+        /** @var EventSubscriberInterface $subscriber */
+        foreach ($this->container->getByInterface(EventSubscriberInterface::class) as $subscriber) {
+            $this->addSubscriber($subscriber);
+        }
+    }
+
+    private function addSubscriber(EventSubscriberInterface $subscriber): void
+    {
+        $this->subscribers[] = $subscriber;
+    }
+
+    private function addListeners(): void
+    {
+        /** @var object&callable $listener */
+        foreach ($this->container->getByTag('event-listener') as $listener) {
             $this->addListener($listener);
         }
     }
 
-    private function addListener(ListenerInterface $listener): void
+    /**
+     * @param object&callable $listener
+     */
+    private function addListener(object $listener): void
     {
-        $this->listeners[] = $listener;
-    }
+        $reflexion = new \ReflectionClass($listener);
 
-    public function getListenersForEvent(Event $event): iterable
-    {
-        foreach ($this->listeners as $listener) {
-            foreach ($listener->getListenedEvents() as $eventClass => $_) {
-                if ($event instanceof $eventClass) {
-                    yield $listener;
-                }
+        $priority = 0;
+        $attributes = $reflexion->getAttributes(EventListener::class);
+
+        foreach ($attributes as $attribute) {
+            $arguments = $attribute->getArguments();
+
+            if (isset($arguments['priority'])) {
+                $priority = $arguments['priority'];
             }
         }
+
+        $invokeMethod = $reflexion->getMethod('__invoke');
+        $parameters = $invokeMethod->getParameters();
+
+        if (1 !== count($parameters)) {
+            throw new InvalidListenerArgumentException('Listener must have only one parameter and it must be an instance of '.Event::class);
+        }
+
+        /**
+         * @var \ReflectionNamedType|null
+         */
+        $eventType = $parameters[0]->getType();
+
+        if ($eventType && is_a($eventType->getName(), Event::class, true)) {
+            $this->listeners[$eventType->getName()][] = [
+                'listener' => $listener,
+                'priority' => $priority,
+            ];
+        }
+    }
+
+    /**
+     * @return callable[]
+     */
+    public function getListenersForEvent(object $event): iterable
+    {
+        $returnedListeners = [];
+
+        if ($event instanceof Event) {
+            foreach ($this->subscribers as $subscriber) {
+                foreach ($subscriber->getSubscribedEvents() as $eventClass => $infos) {
+                    if ($event instanceof $eventClass) {
+                        $returnedListeners = array_merge($this->getListenerInfosFromSubscriberInfos($subscriber, $infos), $returnedListeners);
+                    }
+                }
+            }
+
+            foreach ($this->listeners as $eventClass => $listenersInfos) {
+                if ($event instanceof $eventClass) {
+                    foreach ($listenersInfos as $listenerInfos) {
+                        $returnedListeners = array_merge([['method' => $listenerInfos['listener'](...), 'priority' => $listenerInfos['priority']]], $returnedListeners);
+                    }
+                }
+            }
+
+            $sortedListeners = $this->sortListeners($returnedListeners);
+
+            foreach ($sortedListeners as $listener) {
+                yield $listener['method'];
+            }
+        } else {
+            throw new InvalidArgumentException('Event must be an instance of '.Event::class);
+        }
+    }
+
+    /**
+     * @return array<array{
+     *  method: callable,
+     *  priority: int
+     * }>
+     */
+    private function getListenerInfosFromSubscriberInfos(EventSubscriberInterface $subscriber, mixed $infos): array
+    {
+        $listenersInfos = [];
+
+        if (!is_array($infos)) {
+            $listenersInfos = [
+                [
+                    'method' => $subscriber->$infos(...),
+                    'priority' => 0,
+                ],
+            ];
+        } elseif ($infos[1] && is_int($infos[1])) {
+            $listenersInfos = [
+                [
+                    'method' => $subscriber->{$infos[0]}(...),
+                    'priority' => $infos[1],
+                ],
+            ];
+        } else {
+            foreach ($infos as $info) {
+                $listenersInfos = array_merge($this->getListenerInfosFromSubscriberInfos($subscriber, $info), $listenersInfos);
+            }
+        }
+
+        return $listenersInfos;
+    }
+
+    /**
+     * @param array<array{
+     *  method: callable,
+     *  priority: int
+     * }> $listeners
+     *
+     * @return array<array{
+     *  method: callable,
+     *  priority: int
+     * }>
+     */
+    private function sortListeners(array $listeners): array
+    {
+        usort($listeners, function ($a, $b) {
+            return $b['priority'] <=> $a['priority'];
+        });
+
+        return $listeners;
     }
 }
